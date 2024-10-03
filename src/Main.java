fcac38289b29cecbacacac9f391f95b453d621d5
Mys_Ept <mys_ept@hotmail.com>
2024-10-03 21:49:26 +0800

    newBranch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("李四修改了代码");
-        System.out.println("李四添加了分支，这是在分支上完成的内容");
     }
 }
