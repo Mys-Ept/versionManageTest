41e35514211bf1acf5d419b92c8701d266ef19d1
Mys_Ept <mys_ept@hotmail.com>
2024-10-03 21:51:55 +0800

    newBranch changed by lisi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("李四修改了代码");
+        System.out.println("这是新分支下李华添加的代码");
     }
 }
